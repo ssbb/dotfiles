e77d9de481d7885e8e401e001dae9fcc58ad3374
Sviatoslav Bulbakha <mail@ssbb.me>
2026-01-19 17:08:09 +0400

    Add org-modern

--- stow/emacs/.emacs.d/modules/ssbb-prose.el
+++ stow/emacs/.emacs.d/modules/ssbb-prose.el
@@ -13,8 +13,26 @@
   (org-log-done 'time)
   (org-imenu-depth 100)
   (org-id-method 'org)
+  (org-pretty-entities t)
+  (org-hide-emphasis-markers t)
+  (org-ellipsis "…")
+  (org-insert-heading-respect-content t)
+  (org-special-ctrl-a/e t)
   :config
-   (add-to-list 'org-modules 'org-habit))
+  (add-to-list 'org-modules 'org-habit))
+
+(use-package org-modern-indent
+  :vc (:url "https://github.com/jdtsmith/org-modern-indent")
+  :config
+  (add-hook 'org-mode-hook #'org-modern-indent-mode 90))
+
+(use-package org-modern
+  :custom
+  (org-modern-star 'replace)
+  (org-modern-hide-stars nil)
+  (org-modern-table nil)
+  :config
+  (global-org-modern-mode))
 
 (use-package org-ql)
 
